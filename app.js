a620e3c8c182f1420fdb0465259ca8c2e642df4c
Reagan Bodily <reaganbodily@gmail.com>
2022-03-16 13:46:15 -0600

    captured user input

---
+++ app.js
@@ -0,0 +1,18 @@
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+    
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+  
+  printProfileData(profileDataArgs);
